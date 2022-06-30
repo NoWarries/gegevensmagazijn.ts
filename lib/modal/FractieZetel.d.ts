0b1913a8a4824a11e9224370deefe3c4a9431c26
Coen Warries <coen.warries@student.hu.nl>
2022-06-30 13:17:40 +0200

    backward port lib changes

--- lib/modal/FractieZetel.d.ts
+++ lib/modal/FractieZetel.d.ts
@@ -1,8 +1,11 @@
 import { Deserializer } from '../controller/Deserializer';
+import { Fractie } from '../modal';
 export declare class FractieZetel extends Deserializer {
+    Fractie: Fractie;
     Id: string;
     Gewicht: number;
     Fractie_Id: string;
     ApiGewijzigdOp: Date;
     Verwijderd: boolean;
+    static toString(): string;
 }
