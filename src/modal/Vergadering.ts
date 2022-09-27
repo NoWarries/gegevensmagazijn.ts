74f856a4744dc847b533ed45c29841a52c938ee9
Coen Warries <coen.warries@student.hu.nl>
2022-09-27 19:38:52 +0200

    fix: add required toString & minor style fixes

--- src/modal/Vergadering.ts
+++ src/modal/Vergadering.ts
@@ -2,22 +2,27 @@ import {Deserializer} from '../controller/Deserializer';
 import { Verslag } from '../modal';
 
 export class Vergadering extends Deserializer {
-
     /* Associations */
     /* 1..n */
     Verslag: Verslag;
 
-  Id: string;
-  Soort: string;
-  Titel: string;
-  Zaal: string;
-  Vergaderjaar: string;
-  VergaderingNummer: number;
-  Datum: string;
-  Aanvangstijd: string;
-  Sluiting: string;
-  Kamer: string;
-  GewijzigdOp: string;
-  ApiGewijzigdOp: string;
-  Verwijderd: boolean;
+    /* Attributes */
+    Id: string;
+    Soort: string;
+    Titel: string;
+    Zaal: string;
+    Vergaderjaar: string;
+    VergaderingNummer: number;
+    Datum: string;
+    Aanvangstijd: string;
+    Sluiting: string;
+    Kamer: string;
+    GewijzigdOp: string;
+    ApiGewijzigdOp: string;
+    Verwijderd: boolean;
+
+    /* toString */
+    static toString(): string {
+        return 'Vergadering';
+    }
 }
